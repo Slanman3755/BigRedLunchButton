63ef46f821d169ec9e52baea0a520d6a15348357
Spencer Lanman <slanman@system1.com>
2020-02-05 23:00:25 -0800

    Added time verifaction to requests and modified light behavior to signal when lunch is here

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-# BigRedLunchButton
+# Big Red Lunch Button
 Posts Venice lunch notfications to the System1 slack
 
 Add custom lunch messages by appending new lines to the messages.txt file
+
+## Installation
+- Have a Raspberry Pi connected to an led and a switch in a pull down configuration
+- Clone repository
+- Install required python packages `pip install -r requirements.txt`
+- Copy `config.yaml` to `~/.bigredlunchbutton`
+- Fill out values in yaml config (slack webhooks api url, path to messages.txt, gpio pins)
+- Run script `python button.py`
--- button.py
+++ button.py
@@ -5,12 +5,29 @@ import yaml
 
 from gpiozero import LED, Button
 from pathlib import Path
-from signal import pause
+from pytz import timezone
+from datetime import datetime, time
+
+tz = timezone('US/Pacific')
+valid_window_start = time(11, 0, 0, tzinfo=tz)
+valid_window_end = time(14, 0, 0, tzinfo=tz)
+
+
+def is_in_window(current_time):
+    return valid_window_start <= current_time.time() <= valid_window_end
+
+
+def is_valid_press(current_time, last_time=None):
+    in_window = is_in_window(current_time)
+    if last_time:
+        return current_time.date() != last_time.date() and in_window
+    else:
+        return in_window
 
 
 home_path = str(Path.home())
 config_file_path = f"{home_path}/.bigredlunchbutton"
-log_file_path = f"{home_path}/bigredlunchbutton.log"
+log_file_path = f"/var/log/bigredlunchbutton.log"
 
 logging.basicConfig(
     filename=log_file_path,
@@ -20,7 +37,12 @@ logging.basicConfig(
 )
 
 
+last_posted = None
+
+
 def post_to_slack(api_url, messages):
+    global last_posted
+
     message = messages[random.randint(0, len(messages) - 1)]
     text = "@here " + message
     data = {"text": text, "link_names": 1}
@@ -29,31 +51,36 @@ def post_to_slack(api_url, messages):
     code = response.status_code
 
     if code == 200:
+        last_posted = datetime.now(tz=tz)
         logging.info(f"Posted message to slack: {text}")
     else:
         logging.error(f"Error posting message to slack: {code}\n{response.content}")
 
 
 def listen(button_pin, led_pin, api_url, messages):
-    button = Button(button_pin, pull_up=False)
+    global last_posted
+
+    button = Button(button_pin, pull_up=False, bounce_time=1)
     led = LED(led_pin)
+
     logging.info("Initialized GPIO")
 
     def button_pressed():
         logging.info("Button pressed")
-        led.on()
-        post_to_slack(api_url, messages)
-
-    def button_released():
-        logging.info("Button released")
-        led.off()
-
-    button.when_pressed = button_pressed
-    button.when_released = button_released
+        now = datetime.now(tz=tz)
+        if is_valid_press(now, last_posted):
+            post_to_slack(api_url, messages)
+        else:
+            logging.warning("Invalid request time")
 
     logging.info("Listening...")
-
-    pause()
+    button.when_pressed = button_pressed
+    while True:
+        now = datetime.now(tz=tz)
+        if is_in_window(now) and now.date() == last_posted.date():
+            led.on()
+        else:
+            led.off()
 
 
 def main():
@@ -65,8 +92,7 @@ def main():
         button_pin = config["button_pin"]
         led_pin = config["led_pin"]
         api_url = config["url"]
-        working_dir = config["working_dir"]
-        messages_file_path = f"{working_dir}/messages.txt"
+        messages_file_path = config["messages_file"]
 
         logging.info("Loaded config")
 
